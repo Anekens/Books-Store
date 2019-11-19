20a9d0089d54a2241706d38abb5381b1e63fadd6
Pavel Milenki <p.milenkii@gmail.com>
2019-11-20 02:49:47 +0300

    Updates

---
+++ precache-manifest.5d562863b09b6a40616286125df79775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e4cc70243e803b18a6e6c1bde34870",
+    "url": "/BooksStore/index.html"
+  },
+  {
+    "revision": "5680887d70df9cbe46a0",
+    "url": "/BooksStore/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "7ce84e4e01d99b192669",
+    "url": "/BooksStore/static/js/2.123098c9.chunk.js"
+  },
+  {
+    "revision": "5680887d70df9cbe46a0",
+    "url": "/BooksStore/static/js/main.10bc1e20.chunk.js"
+  },
+  {
+    "revision": "4b49c4403dfb57cc843c",
+    "url": "/BooksStore/static/js/runtime-main.f9c6d1b4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/BooksStore/static/media/logo.25bf045c.svg"
+  }
+]);
