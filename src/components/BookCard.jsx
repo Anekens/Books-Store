9e57af982ed15d91bfc305d9b478f3170c9cd749
Pavel Milenki <54291798+PavelMilenki@users.noreply.github.com>
2020-01-08 16:13:51 +0300

    added css fix

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
-import { Card, Image, Icon, Button } from 'semantic-ui-react';
+import {Card, Image, Icon, Button} from 'semantic-ui-react';
 
 const BookCard = (book) => {
-   return (
-    <Card>
-      <div className="card-image">
-        <Image src={book.imageLink} />
-      </div>
-      <Card.Content>
-        <Card.Header>{book.title}</Card.Header>
-        <Card.Meta>
-          <span className="date">{book.author}</span>
-        </Card.Meta>
-      </Card.Content>
-      <Card.Content extra>
-        <a>
-          <Icon name="dollar" />
-          {book.price}
-        </a>
-      </Card.Content>
-      <Button onClick={book.addToCart.bind(this, book)}>
-          Add to cart {book.addedCount > 0 && `(${book.addedCount})`}
-      </Button>
-    </Card>
-  );
+    return (
+        <div className='books'>
+            <Card>
+                <div className="card-image">
+                    <Image src={book.imageLink}/>
+                </div>
+                <Card.Content>
+                    <Card.Header>{book.title}</Card.Header>
+                    <Card.Meta>
+                        <span className="date">{book.author}</span>
+                    </Card.Meta>
+                </Card.Content>
+                <Card.Content extra>
+                    <a>
+                        <Icon name="dollar"/>
+                        {book.price}
+                    </a>
+                </Card.Content>
+                <Button onClick={book.addToCart.bind(this, book)}>
+                    Add to cart {book.addedCount > 0 && `(${book.addedCount})`}
+                </Button>
+            </Card>
+        </div>
+    );
 };
 
 export default BookCard;
